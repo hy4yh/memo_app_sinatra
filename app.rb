bc5b7a1ed92eab589e5b99bedd2203bb9a355a02
hy4yh <hy4yh@users.noreply.github.com>
2019-01-26 10:54:18 +0900

    メモの内容の取得、memo.rb作成

--- app.rb
+++ app.rb
@@ -13,3 +13,14 @@ end
 get '/memos/new' do
   erb :'memos/new'
 end
+
+post '/memos' do
+  @memo_title = params[:memo_contents].lines[0]
+  @memo_content = params[:memo_contents].lines[1..-1].join
+  if @memo_title.gsub(/^[[:space:]]+/, '').empty?
+    puts "空白文字のみのタイトルは禁止されています。"
+  else
+    #csvファイルへの保存処理
+  end
+  redirect '/memos/:id'
+end
